53c49e44d74d58693228db1ee9fe72ef1182a265
Manj <manj_man@hotmail.co.uk>
2024-08-26 11:46:44 +0100

    Configured absolute paths config

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,7 +1,10 @@
-<script>
+<script lang="ts">
     export let title = "Default Title";  // You can pass a title as a prop
+    import Navigation from '@/components/Navigation.svelte';
   </script>
   
   <header class="bg-blue-600 text-white p-4">
+    <Navigation />
+
     <h1 class="text-2xl font-bold">{title}</h1>
   </header>
